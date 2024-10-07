ecace9fec7797fa8869eeefb484a3350663dffed
Guilherme Batista <58760655+Guib23@users.noreply.github.com>
2024-10-07 19:53:20 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
+import logging
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Executando função de teste.')
+    return func.HttpResponse("Função rodou com sucesso!", status_code=200)
+
 print('Hello Azure')
